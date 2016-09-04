842959d3c19a70187d9ec1b72249e752c5d62890
Gautier Nilhcem <nilhcem@gmail.com>
2016-09-04 20:36:56 +0200

    Use localhost url for dockerised sonarqube

--- scripts/run-sonarqube-analysis.sh
+++ scripts/run-sonarqube-analysis.sh
@@ -5,4 +5,4 @@ export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
 
 ./gradlew clean :app:assembleProductionDebug :app:testProductionDebugUnitTest :app:sonarqube
 
-echo "SonarQube reports: http://docker:9000"
+echo "SonarQube reports: http://localhost:9000"
